6385913c224ec3a320ee1b905a095042a214ac7b
Xiaofeng Wang <xwang@webmd.net>
2018-12-19 23:03:23 -0500

    2018/12/19 完成

---
+++ algorithms/014LongestCommonPrefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    var longest='';
+    var l=strs.length;
+    longest=strs[0];
+    for(var i=1;i<l;i++){
+        // current=
+        for(var j=0,lon=longest.length;j<lon;j++){
+            if(longest[j]!==strs[i][j]){
+                longest=longest.slice(0,j);
+                break;
+            }
+        }
+    }
+    return longest;
+
+
+
+};
